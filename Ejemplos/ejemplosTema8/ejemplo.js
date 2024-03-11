c5bc843856902ed8b60971f919c8f4bb2f6aafed
Fran <alcantarilla.cafra24@triana.salesianos.edu>
2024-03-11 10:00:46 +0100

    a

---
+++ Ejemplos/ejemplosTema8/ejemplo.js
@@ -0,0 +1,9 @@
+let mes = prompt("Indica un mes"); //Propmt es como si pidieras por teclado
+alert("Estamos en "+mes);
+
+
+//let enlace = '<a href="./http://127.0.0.1:5500/Ejemplos/ejemplosTema8/ejemplojs.html">' //Por si hace falta poner un documento html en el codigo
+
+
+/*Arrays*/
+//let dia = new dias("Lunes",0,"Martes"); 
