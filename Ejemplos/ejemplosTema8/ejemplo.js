2cb660e6a2b64c2e71d104a3ec52f939ef0a2782
Fran <alcantarilla.cafra24@triana.salesianos.edu>
2024-03-14 09:05:22 +0100

    a

--- Ejemplos/ejemplosTema8/ejemplo.js
+++ Ejemplos/ejemplosTema8/ejemplo.js
@@ -1,5 +1,5 @@
-let mes = prompt("Indica un mes"); //Propmt es como si pidieras por teclado
-alert("Estamos en "+mes);
+/*let mes = prompt("Indica un mes"); //Propmt es como si pidieras por teclado
+alert("Estamos en "+mes);*/
 
 
 //let enlace = '<a href="./http://127.0.0.1:5500/Ejemplos/ejemplosTema8/ejemplojs.html">' //Por si hace falta poner un documento html en el codigo
@@ -8,7 +8,28 @@ alert("Estamos en "+mes);
 /*Arrays*/
 //let dia = new dias("Lunes",0,"Martes"); 
 
-let meses= ['ene','feb','mar','abr']
+/*let meses= ['ene','feb','mar','abr']
 for(let mes of meses){
     alert (mes);
-}
+} */
+
+//let valores = [true, 5, false, "hola", "adios", 2];
+
+
+let mensaje = "hola hola vecinito"
+//let letra = mensaje.charAt(5);
+//alert(letra)
+//-------------------------------------------------------------------------
+//alert(mensaje.substring(5,8)); 
+//Esto coge el rango de caracateres que esten dentro de ese 
+//rango mostrando uno antes del que es el ultimo de rango
+
+
+
+//----------------------------------------------------------------------
+
+//Esto es para separar palabras 
+/*let partes = mensaje.split ("")
+for (let parte of partes){
+    alert(parte);
+}*/
--- Ejercicios3ºTrimestres/ejerciciosUD8/ejercicio4/js/ejercicio4.js
+++ Ejercicios3ºTrimestres/ejerciciosUD8/ejercicio4/js/ejercicio4.js
@@ -1,2 +1,14 @@
 let valores = [true, 5, false, "hola", "adios", 2];
-alert(valores);
+
+let mayor = (valores[3]>valores[4])?valores[3]:valores[4];
+alert('el mayor es '+mayor);
+
+
+alert(valores[0]||valores[2]);
+alert(valores[0]&&valores[2]);
+alert(valores[1]+valores[5]);
+alert(valores[1]-valores[5]);
+alert(valores[1]*valores[5]);
+alert(valores[1]/valores[5]);
+alert(valores[1]%valores[5]);
+
