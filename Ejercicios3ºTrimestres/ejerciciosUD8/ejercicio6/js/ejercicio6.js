0e3661180c2722fecd43015d6f37cfd7be91d701
Francisco José Alcantarilla Calado <alcantarilla.cafra24@triana.salesianos.edu>
2024-03-18 22:39:58 +0100

    a

---
+++ Ejercicios3ºTrimestres/ejerciciosUD8/ejercicio6/js/ejercicio6.js
@@ -0,0 +1,13 @@
+
+let veintitres = 23;
+let numeroDni = prompt("Introduzca el numero del dni")
+let letraDni =  ['T','R','W','A','G','M','T','F','P','D','X','B','N'
+                ,'J','Z','S','Q','V','H','L','C','K'];
+letraDni = prompt("Seleccione la letra del dni")
+
+
+if (numeroDni < 0 && numeroDni > 99999999 ) {
+
+    
+}
+
---
+++ Ejercicios3ºTrimestres/ejerciciosUD8/ejercicio7/js/ejercicio7.js
@@ -0,0 +1,11 @@
+let numFactorial =  prompt("Introduzca el numero del que quieres sacar el factorial")
+let resultado;
+
+for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    
+}
+
+
+
+alert(resultado)
