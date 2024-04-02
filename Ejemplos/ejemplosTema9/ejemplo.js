545f2655b9155b11553a2c620e46039c5780b41d
Fran <alcantarilla.cafra24@triana.salesianos.edu>
2024-04-02 10:59:42 +0200

    a

--- Ejemplos/ejemplosTema9/ejemplo.js
+++ Ejemplos/ejemplosTema9/ejemplo.js
@@ -8,7 +8,7 @@ for(let el of elementos){
     el.innerText += "leido";
 }*/
 
-let boton = document.getElementById('boton')
+/*let boton = document.getElementById('boton')
 boton.addEventListener('click', botonPulsado)
 
 function botonPulsado() {
@@ -16,4 +16,73 @@ function botonPulsado() {
   for(li of primerLi){
     alert(li.innerText)
   }
+}*/
+//-------------------------------------------------------------------------------
+
+let boton = document.getElementById('boton')
+boton.addEventListener('click', botonPulsado)
+
+
+function botonPulsado() {
+
+ /* 
+  let texto  = prompt("Diga el texto del nuevo elemento")
+
+  //obtengo la lista
+  let lista =  document.querySelector('ol')
+
+  //Creamos el elemento
+  let nuevoLi = document.createElement('li')
+
+  //le damos contenido
+  nuevoLi.innerText=texto;
+
+  //añado el li al ol
+  lista.append(nuevoLi)
+  
+  */
+
+
+//------------------------------------------------------------------------------
+/*
+let zonaMensajes = document.querySelector('.zonaMensajes');
+
+let nuevoP = zonaMensajes.cloneNode(true);
+
+nuevoP.querySelector('strong').innerText = 1;
+
+zonaMensajes.after(nuevoP)*/
+
+//-------------------------------------------------------------------
+/*
+let zonaMensajes = document.querySelector('.zonaMensajes')
+
+let numMensajes = zonaMensajes.querySelector('strong')
+
+let numActual = parseInt(numMensajes.innerText)+1;
+numMensajes.innerText = numActual;
+*/
+
+
+
+//------------------------------------------------------------------------------
+
+
+/*
+
+let lista =  document.querySelector('ol')
+
+let segundoLi = lista.querySelector('li:nth-child(2)');
+
+
+lista.append(segundoLi)
+
+*/
+
+
+//---------------------------------------------------------------------------------
+
+let zonaMensajes = document.querySelector('.zonaMensajes')
+zonaMensajes.hidden = !zonaMensajes.hidden
+
 }
